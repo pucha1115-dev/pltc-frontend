7ef804b3d56fb28d608e9b690cd60f88443491c4
jp-bns2020 <jp.bns2020@gmail.com>
2024-06-18 23:54:20 +0800

    add pagination in frontend

--- src/pages/SimMasterList.jsx
+++ src/pages/SimMasterList.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { COLORS } from "../constants";
 import {
   Table,
@@ -11,63 +13,90 @@ import {
   Button,
   Stack,
   Box,
-  FormControl
+  FormControl,
+  Text,
+  Select,
 } from "@chakra-ui/react";
 import TableRowDisplaySim from "../components/TableRowDisplaySim";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-
 
 const SimMasterList = () => {
   const [simList, setSimList] = useState([]);
-  const [filteredSimList, setFilteredSimList] = useState(simList);
+  const [filteredSimList, setFilteredSimList] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(20); // Adjust items per page as needed
+  const [totalRecords, setTotalRecords] = useState(0); // Total number of records
   const navigate = useNavigate();
 
   useEffect(() => {
     getSimList();
-  }, []);
+  }, [currentPage]); // Fetch data when currentPage changes
 
   const getSimList = async () => {
     try {
-      const response = await axios.get("http://localhost:8000/api/sims/");
+      const response = await axios.get(`http://localhost:8000/api/sims/`);
       if (response.status === 200) {
-        setSimList(response.data);
-        setFilteredSimList(response.data);
+        const data = response.data;
+        setSimList(data); // Set simList with fetched data
+        if (searchValue !== "") {
+          // If there's a search value, filter simList
+          filterSimList(data);
+        } else {
+          // Otherwise, set filteredSimList to the entire simList
+          setFilteredSimList(data);
+        }
       }
     } catch (error) {
       alert(error.message);
     }
   };
 
-  const handleSimClick = (sim) => {
-    const data = {sim: sim}
-    navigate('/sim',{state:{data}})
-
-  } 
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-    if (searchValue === "") {
-      setFilteredSimList(simList);
-      return;
-    }
-
-    
-    const filterBySearch = simList.filter(
+  const filterSimList = (list) => {
+    const filterBySearch = list.filter(
       (item) =>
         item.min_hp_number === searchValue ||
         item.iccid === searchValue ||
-        item.carrier.toLowerCase() === searchValue.toLowerCase()  ||
+        item.carrier.toLowerCase() === searchValue.toLowerCase() ||
         item.apn.toLowerCase() === searchValue.toLowerCase() ||
         item.status.toLowerCase() === searchValue.toLowerCase() ||
         item.username.toLowerCase() === searchValue.toLowerCase() ||
         item.ip === searchValue
     );
-    setFilteredSimList(filterBySearch);
+    setFilteredSimList(filterBySearch); // Update filteredSimList based on search criteria
+    setTotalRecords(filterBySearch.length);
   };
+
+  const handleSimClick = (sim) => {
+    const data = { sim: sim };
+    navigate("/sim", { state: { data } });
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setCurrentPage(1);
+    if (searchValue === "") {
+      // If searchValue is empty, reset to show all simList
+      setFilteredSimList(simList);
+      setTotalRecords(simList.length);
+    } else {
+      // Filter simList based on search criteria
+      filterSimList(simList);
+    }
+  };
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber); // Update currentPage when page is changed
+  };
+
+  // Calculate current items to display based on pagination
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredSimList.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Calculate total number of pages
+  const pageCount = Math.ceil(filteredSimList.length / itemsPerPage);
 
   return (
     <Flex
@@ -79,29 +108,42 @@ const SimMasterList = () => {
       width="calc(100vw - 200px)"
       flexDir="column"
       marginLeft="200px"
-    > <FormControl>
-      <Stack direction="row" mb={5} justifyContent='space-between'>
-      <Box display='flex' justifyContent='center'>
-        <Input
-          size='sm'
-          border={0}
-          mr={2}
-          backgroundColor='white'
-          color='black'
-          borderRadius={4}
-          placeholder="Search..."
-          w="280px"
-          onChange={e => setSearchValue(e.target.value)}
-        ></Input>
-        <Button type="submit" size='sm' width='100px' backgroundColor={COLORS.TEXT} onClick={handleSearch}>Search</Button>
-        </Box>
-        <Button size='sm' bg='green' color='white' mr={5}><Link to='/sim'>Add SIM</Link></Button>
-      </Stack>
-
-     
+    >
+      <FormControl>
+        <form onSubmit={handleSearch}>
+          <Stack direction="row" mb={5} justifyContent="space-between">
+            <Box display="flex" justifyContent="">
+              <Input
+                size="sm"
+                border={0}
+                mr={2}
+                backgroundColor="white"
+                color="black"
+                borderRadius={4}
+                placeholder="Search..."
+                w="280px"
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+              />
+              <Button
+                type="submit"
+                size="sm"
+                width="100px"
+                backgroundColor={COLORS.TEXT}
+              >
+                Search
+              </Button>
+              <Text ml={10} alignSelf="flex-end">
+                Records Found: {totalRecords}
+              </Text>
+            </Box>
+            <Button size="sm" bg="green" color="white" mr={5}>
+              <Link to="/sim">Add SIM</Link>
+            </Button>
+          </Stack>
+        </form>
       </FormControl>
-
-      <TableContainer borderBottom="1px solid" width="100%" overflowY="scroll">
+      <TableContainer borderBottom="1px solid" width="100%">
         <Table size="sm" variant="unstyled">
           <Thead
             position="sticky"
@@ -137,24 +179,56 @@ const SimMasterList = () => {
             </Tr>
           </Thead>
           <Tbody>
-            {filteredSimList.map((sim, index) => (
+            {currentItems.map((sim, index) => (
               <TableRowDisplaySim
-              key={index}
-              iccid={sim.iccid}
-              min_hp={sim.min_hp_number}
-              ip={sim.ip}
-              apn={sim.apn}
-              username={sim.username}
-              password={sim.password}
-              carrier={sim.carrier}
-              status={sim.status}
-              onClick={() => handleSimClick(sim)}
-            />
-              
+                key={index}
+                iccid={sim.iccid}
+                min_hp={sim.min_hp_number}
+                ip={sim.ip}
+                apn={sim.apn}
+                username={sim.username}
+                password={sim.password}
+                carrier={sim.carrier}
+                status={sim.status}
+                onClick={() => handleSimClick(sim)}
+              />
             ))}
           </Tbody>
         </Table>
       </TableContainer>
+      <Stack direction="row" alignItems="end" justifyContent="center" mt={4}>
+        <Button
+          bg={COLORS.ACCENT}
+          size="sm"
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </Button>
+        <Select
+          value={currentPage}
+          onChange={(e) => handlePageChange(parseInt(e.target.value))}
+          size="sm"
+          width="80px"
+          bg="white"
+          color="black"
+          border={0}
+        >
+          {Array.from({ length: pageCount }, (_, i) => (
+            <option key={i + 1} value={i + 1}>
+              {i + 1}
+            </option>
+          ))}
+        </Select>
+        <Button
+          bg={COLORS.ACCENT}
+          size="sm"
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === pageCount || pageCount === 0}
+        >
+          Next
+        </Button>
+      </Stack>
     </Flex>
   );
 };
--- src/pages/UploadSimCsv.jsx
+++ src/pages/UploadSimCsv.jsx
@@ -1,14 +1,18 @@
 import { Input, Button } from "@chakra-ui/react";
 import axios from "axios";
 import { useState } from "react";
-import {COLORS} from '../constants'
+import { COLORS } from "../constants";
 
 const UploadSimCsv = () => {
   const [file, setFile] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleUpload = async () => {
     const formData = new FormData();
     formData.append("file", file);
+
+    setLoading(true);
+
     try {
       const response = await axios.post(
         "http://localhost:8000/api/sims/upload-csv/",
@@ -17,9 +21,11 @@ const UploadSimCsv = () => {
       );
       if (response.status === 201) {
         alert("File upload successful.");
+        setLoading(false);
       }
     } catch (error) {
       alert(error.message);
+      setLoading(false);
     }
   };
 
@@ -29,17 +35,34 @@ const UploadSimCsv = () => {
 
   return (
     <>
-      <Input 
-      width='220px' 
-      alignItems='center' 
-      justifyContent='center' 
-      size='sm' 
-      border={0} 
-      type="file" 
-      onChange={onFileChange}>
-        
-      </Input>
-      <Button  size='sm' backgroundColor={COLORS.ACCENT}  onClick={handleUpload}>Upload</Button>
+      <Input
+        width="220px"
+        alignItems="center"
+        justifyContent="center"
+        size="sm"
+        border={0}
+        type="file"
+        onChange={onFileChange}
+      ></Input>
+
+      {loading ? (
+        <Button
+          size="sm"
+          isLoading
+          backgroundColor={COLORS.ACCENT}
+          onClick={handleUpload}
+        >
+          Upload
+        </Button>
+      ) : (
+        <Button
+          size="sm"
+          backgroundColor={COLORS.ACCENT}
+          onClick={handleUpload}
+        >
+          Upload
+        </Button>
+      )}
     </>
   );
 };
